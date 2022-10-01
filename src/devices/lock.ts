be932d723075ea36629e0d5bcf01cd5e458427a4
Donavan Becker <beckersmarthome@icloud.com>
2022-10-01 15:22:20 -0500

    Update lock.ts

--- src/devices/lock.ts
+++ src/devices/lock.ts
@@ -249,6 +249,12 @@ export class LockMechanism {
       } else {
         this.errorLog(`Lock: ${this.accessory.displayName} lockStatus (pushChanges) failed, this.LockTargetState: ${this.LockTargetState}`);
       }
+
+      interval(this.updateRate * 1000)
+        .pipe(take(2))
+        .subscribe(async () => {
+          await this.refreshStatus();
+        });
     } catch (e: any) {
       this.errorLog(e);
     }
